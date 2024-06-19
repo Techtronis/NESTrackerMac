cf8ef07e7074a11b08a32fe580fc5617c7cf5965
Techtronis <108685009+Techtronis@users.noreply.github.com>
2024-06-19 22:20:31 +0200

    Initial Commit

---
+++ NESTrackerMac/NESTrackerMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  NESTrackerMacApp.swift
+//  NESTrackerMac
+//
+//  Created by Alexis Jost on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct NESTrackerMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NESTrackerMacTests/NESTrackerMacTests.swift
@@ -0,0 +1,17 @@
+//
+//  NESTrackerMacTests.swift
+//  NESTrackerMacTests
+//
+//  Created by Alexis Jost on 19.06.2024.
+//
+
+import Testing
+@testable import NESTrackerMac
+
+struct NESTrackerMacTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
